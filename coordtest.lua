c8b532d944c00ca2fe9f65efa4bbac46afac167c
drert <54976369+drert@users.noreply.github.com>
2023-04-05 18:29:45 -0400

    asdf
    asdf

---
+++ coordtest.lua
@@ -0,0 +1,56 @@
+x,y,z = gps.locate()
+facing = 0 -- direction facing, 0 = start (north), 1-3 clockwise
+
+function report() 
+
+end
+
+function CC() 
+    turtle.turnRight()
+    facing = (facing + 1) % 4
+end
+
+function CCW()
+    turtle.turnRight()
+    facing = (facing - 1) % 4
+end
+
+function DOWN(dist)
+    for i = 1, dist, 1 do
+        turtle.digDown()
+        turtle.down()
+    end
+end
+
+function UP(dist)
+    for i = 1, dist, 1 do
+        turtle.digUp()
+        turtle.up()
+    end
+end
+
+function FW(dist)
+    for i = 1, dist, 1 do
+        turtle.dig()
+        turtle.forward()
+    end
+end
+
+function Home() 
+    write("Turtle Returning Home")
+end
+
+function checkInvFull()
+    for i=1, 16, 1 do
+        turtle.select(i)
+        ct = turtle.getItemCount()
+        if ct == 0 then 
+            write("Inventory is not full.")
+            return false
+        end
+    end
+    write("Inventory is full.")
+    return true
+end
+
+checkInvFull()
